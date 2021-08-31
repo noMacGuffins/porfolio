de31315d23f6d97748b054e91b2b6d6bb1969d81
Sehan MacPro <ianparkfirst@gmail.com>
2021-08-31 18:19:27 +0900

    feat: eugene wants responsive

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap'
+import { Navbar, Container, Nav, NavDropdown, Carousel } from 'react-bootstrap'
 import Col from 'src/components/Col'
 import Div from 'src/components/Div'
 import Row from 'src/components/Row'
@@ -10,7 +10,19 @@ import mjProfile from 'public/images/1.png'
 import ericProfile from 'public/images/2.png'
 import eugeneProfile from 'public/images/3.png'
 import chiiProfile from 'public/images/4.png'
-import { useState } from 'react'
+import pic1 from 'public/images/1.png'
+import pic2 from 'public/images/2.png'
+import pic3 from 'public/images/3.png'
+import pic4 from 'public/images/4.png'
+import pic5 from 'public/images/5.png'
+import pic6 from 'public/images/6.png'
+import pic7 from 'public/images/7.png'
+import pic8 from 'public/images/8.png'
+import pic9 from 'public/images/9.png'
+import pic10 from 'public/images/10.png'
+import pic11 from 'public/images/11.png'
+import { useState, useRef } from 'react'
+import ReactCardCarousel from "react-card-carousel";
 
 
 const Discord = () => {
@@ -49,60 +61,36 @@ const Twitter = () => {
     )
 }
 
-const Logo = () => {
+const Logo = (props) => {
+
+    const {width, height, fill} = props
+
     return(
-    <svg width="130px" height="79px" viewBox="0 0 130 79" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
-        <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
-            <g id="Desktop-demo" transform="translate(-25.000000, -8.000000)">
-                <rect id="Rectangle-Copy-18" fill="#141513" x="25" y="8" width="130" height="79" rx="10"></rect>
-                <text id="“Logo”" fontFamily="Phosphate" fontSize="16" fontWeight="normal" letterSpacing="0.8" fill="#4D504B">
-                    <tspan x="58.996" y="49">“Logo”</tspan>
-                </text>
-            </g>
-        </g>
-    </svg>
+        <svg xmlns="http://www.w3.org/2000/svg" fill={fill || "white"} viewBox="0 0 464.24 512.02" width={width || 100} height={height || 100}><defs><style>.cls-1</style></defs><g id="Layer_2" data-name="Layer 2"><path  d="M267.42,178.69" transform="translate(-23.88)"/><path d="M252.2,34.93c7.23-13.79,14.53-22.47,19.69-33.79.13-.35-.24-1.8-2.19-.78-24.08,12.71-41.38,38.22-65,60.81-30.16,3.64-56.41,10.37-75.84,19.14-13.73-13.41-35-19.23-55.95-13.42-30.29,8.41-49.26,39.79-41.39,67.78,5.34,19,21.8,31,41.83,33.47C61.71,202.42,37.29,278.62,27.06,307.46c-2.31,6.51-5,11.6-1.41,21C32.4,346,172.08,512.24,256,512c83.92.22,223.6-166.06,230.35-183.55,3.63-9.41.9-14.5-1.41-21-10.23-28.84-34.65-105-46.28-139.32,20-2.44,36.49-14.46,41.83-33.47,7.87-28-11.1-59.37-41.39-67.78-21-5.81-42.22,0-56,13.42-19.41-8.77-45.62-15.48-75.75-19.12,1.44-3.75,5-7.27,5-11.26,0-.23-.89-2.4-3.2-1-2.53,1.48-10.2,5.42-13.35,5.56-.27,0-.6-.41-.4-1.06.92-3.09,6.17-13.06,8.88-23,.09-.31,0-1.09-3-.42a87.93,87.93,0,0,0-19.21,6.83c-5.61,2.65,5-13.6,8.05-21.14.74-1.8-3.15.25-3.15.25ZM166.67,163.42c-16.76-2.2-23.13-18.23-23.13-18.23l45.51,8S176.86,164.76,166.67,163.42ZM256,303c-18.41,0-63.95-10.48-63.95-64.33,0-30.69,23.81-72.57,63.95-72.64S320,208,320,238.7C320,292.55,274.41,303,256,303Zm89.33-139.61C335.14,164.76,323,153.18,323,153.18l45.51-8S362.09,161.22,345.33,163.42Z" transform="translate(-23.88)"/><path d="M279.9,222.52c-7-.93-13.22-1.47-17.55-1.77v-7.32c8.56,3.55,16.85.08,16.85-7.42,0-9.69-10.39-17.56-23.2-17.56h0c-12.81,0-23.2,7.87-23.2,17.56,0,7.5,8.29,11,16.85,7.42v7.32c-4.33.3-10.5.84-17.55,1.77-11.65,1.55-13.54,4.84-13.81,8.12,0,.53,1.24,1.7,2.27,1.9,1.54.32,3.66.25,7.49-1.09C236.13,228.62,256,229,256,229h0s19.87-.33,27.95,2.5c3.83,1.34,5.95,1.41,7.49,1.09,1-.2,2.31-1.37,2.27-1.9C293.44,227.36,291.55,224.07,279.9,222.52Z" transform="translate(-23.88)"/></g></svg>
     )
 }
 
-const TopNav = () => {
+const MobileFriendlyNav = ({callBack}) => {
     return(
-        <Row  color="white" px={"15%"}>
-            <Col auto><Logo></Logo></Col>
-            <Col auto textWhite itemsCenter hFull flex>About</Col>
-            <Col auto textWhite itemsCenter hFull flex>Rarity</Col>
-            <Col auto textWhite itemsCenter hFull flex>Re-Roll</Col>
-            <Col auto textWhite itemsCenter hFull flex>C.A.R.P</Col>
-            <Col auto textWhite itemsCenter hFull flex>Team</Col>
-            <Col auto textWhite itemsCenter hFull flex>FAQ</Col>
-        </Row>
-    )
-}
-
-const MobileFriendlyNav = () => {
-    return(
-        <Nav
-            style={{backgroundColor: "#232323", color: "white", fontFamily: "Bungee Inline", position: "sticky", top: 0, zIndex:100, padding: "1% 15% 1% 15%",}}
-            onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
-        >
-            <Nav.Item>
-                <Nav.Link style={{color: "white"}}>About</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link  style={{color: "white"}}>Rarity</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link  style={{color: "white"}}>Re-Roll</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link style={{color: "white"}} >C.A.R.P</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link style={{color: "white"}}>Team</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link  style={{color: "white"}}>FAQ</Nav.Link>
-            </Nav.Item>
-        </Nav>
+        <Navbar bg="light" expand="lg" style={{color: "black", fontFamily: "Bungee Inline", position: "sticky", top: 0, zIndex:100, padding: "1% 15% 1% 15%",}}>
+        <Container fluid>
+            <Div mr5><Logo fill={"black"} width={50} height={50}></Logo></Div>
+            <Navbar.Brand href="#home">
+                BearDom
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+                <Nav.Link href="#about" onClick={() => callBack(0)}>About</Nav.Link>
+                <Nav.Link href="#rarity" onClick={() => callBack(1)}>Rarity</Nav.Link>
+                <Nav.Link href="#reroll" onClick={() => callBack(2)}>Re-Roll</Nav.Link>
+                <Nav.Link href="#carp" onClick={() => callBack(3)}>C.A.R.P</Nav.Link>
+                <Nav.Link href="#team" onClick={() => callBack(4)}>Team</Nav.Link>
+                <Nav.Link href="#faq" onClick={() => callBack(5)}>FAQ</Nav.Link>
+            </Nav>
+            </Navbar.Collapse>
+        </Container>
+        </Navbar>
     )
 }
  
@@ -156,25 +144,33 @@ const BottomMenu = () => {
 
 const Home: NextPage = () => {
     const bodyFontFamily = "Bungee Inline"
+    const aboutRef = useRef(null)
+    const rarityRef = useRef(null)
+    const rerollRef = useRef(null)
+    const carpRef = useRef(null)
+    const teamRef = useRef(null)
+    const faqRef = useRef(null)
+    const refs = [aboutRef, rarityRef, rerollRef, carpRef, teamRef, faqRef]
+    const scrollToRef = (index) => {window.scrollTo(0, refs[index].current.offsetTop - 130)}
 
-  const faqQuestion = [
-    {
-        question: "How on earth did you optimize gas for your nonfungible tokens?!?!!",
-        answer: "dunno"
-    },
-    {
-        question: "How on earth did you optimize gas for your nonfungible tokens?!?!!",
-        answer: "dunno"
-    },
-    {
-        question: "How on earth did you optimize gas for your nonfungible tokens?!?!!",
-        answer: "dunno"
-    },
-    {
-        question: "How on earth did you optimize gas for your nonfungible tokens?!?!!",
-        answer: "dunno"
-    },
-  ]
+    const faqQuestion = [
+        {
+            question: "How on earth did you optimize gas for your nonfungible tokens?!?!!",
+            answer: "dunno"
+        },
+        {
+            question: "How on earth did you optimize gas for your nonfungible tokens?!?!!",
+            answer: "dunno"
+        },
+        {
+            question: "How on earth did you optimize gas for your nonfungible tokens?!?!!",
+            answer: "dunno"
+        },
+        {
+            question: "How on earth did you optimize gas for your nonfungible tokens?!?!!",
+            answer: "dunno"
+        },
+    ]
 
   const [faqExpand, setFaqExpand] = useState([false, false, false, false])
 
@@ -187,18 +183,70 @@ const Home: NextPage = () => {
     setFaqExpand(nextExpandState)
   }
 
+    const carouselStyle = {
+      position: "relative",
+      backgroundImage: bannerImage,
+      backgroundColor: "rgb(248,249,250)",
+      height: "700px",
+      width: "100%",
+      display: "flex",
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "middle"
+    }
+
+    const cardStyle = {
+        height: "500px",
+        width: "500px",
+        textAlign: "center",
+        color: "#FFF",
+        fontFamily: "sans-serif",
+        fontSize: "12px",
+        textTransform: "uppercase",
+        borderRadius: "10px",
+        boxSizing: "border-box"
+    }
+
   return (
     <Div className="container-fluid p-0" style={{backgroundColor: "#30251F", fontFamily: "Bungee Inline"}} letterSpacing={2}>
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com"/>
         <link href={`https://fonts.googleapis.com/css2?family=Bungee+Inline&display=swap`} rel="stylesheet"/>
-        <MobileFriendlyNav></MobileFriendlyNav>
+        <MobileFriendlyNav callBack={scrollToRef}></MobileFriendlyNav>
         <Div h={"100%"} >
-            <Image alt="" src={bannerImage}  height={"2000px"} ></Image>
+            {/* <Image alt="" src={bannerImage}  height={"2000px"} ></Image> */}
+            {/* <Carousel>
+                {[pic1, pic2, pic3, pic4, pic5, pic6, pic7, pic8, pic9, pic10, pic11].map((imageSrc, index) => {
+                    return(
+                        
+                        <Carousel.Item key={index} style={{flex}}>
+                            <Image alt="loading" src={imageSrc} ></Image>
+                            <Carousel.Caption>
+                            <h3>First slide label</h3>
+                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                    )
+                })}
+            </Carousel> */}
+            <Div style={carouselStyle}>
+                <ReactCardCarousel autoplay={true} autoplay_speed={2500}>
+                {[pic1, pic2, pic3, pic4, pic5, pic6, pic7, pic8, pic9, pic10, pic11].map((imageSrc, index) => {
+                    return(
+                        <Div key={index} style={cardStyle}>
+                            <Image alt="loading" src={imageSrc} ></Image>
+                            <Div itemsCenter justifyCenter auto fontSize40 fontFamily={"Bungee Inline"} flex>
+                                <Div bgColor={"#393937"} color={"#FFE058"} rounded20 w317 textCenter>Mint</Div>
+                            </Div>
+                        </Div>
+                    )
+                })}
+                </ReactCardCarousel>
+            </Div>
             <Div px={"15%"} py50>
-                <Row color={"#D3D3D3"}>
+                <Row color={"#D3D3D3"} >
                     <Col mb50>
-                        <Div fontSize40 pb36>WELCOME TO BEARDOM</Div>
+                        <Div fontSize40 pb36 ref={aboutRef}>WELCOME TO BEARDOM</Div>
                         <Div fontSize18 fontFamily={bodyFontFamily}>
                             Beardom is a collection of 10,000 Bear NFTs designed to move the NFT space forward. In addition to providing value as an avatar project, Beardom is a platform for future Dapp extensions. The first is our community artist royalty program (programmed into the genesis smart contract). Future drops/ideas such as follow-up NFT projects will conform to this standard and be directly linked on-chain to, and from, the Beardom contract.
                         </Div>
@@ -239,7 +287,7 @@ const Home: NextPage = () => {
                 <Row h135></Row>
                 
                 <Row color={"#D3D3D3"} >
-                    <Div fontSize40 pb30 textCenter>RARITY / TRAITS (TBD)</Div>
+                    <Div fontSize40 pb30 textCenter ref={rarityRef}>RARITY / TRAITS (TBD) </Div>
                     <Row >
                         <Col >
                             <Div border border5 borderBlack h200></Div>
@@ -252,10 +300,10 @@ const Home: NextPage = () => {
                         </Col>
                     </Row>
                 </Row>
-                <Div hrTag h2 bgColor={"#605E5E"} m50></Div>
+                <Div hrTag h2 bgColor={"#605E5E"} m50 ></Div>
                 <Row color={"#D3D3D3"}>
                     <Col mb50>
-                        <Div fontSize40 pb36>Trait Re-rolling</Div>
+                        <Div fontSize40 pb36 ref={rerollRef}>Trait Re-rolling</Div>
                         <Div fontSize18 fontFamily={bodyFontFamily}>
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit sequi iusto placeat voluptatem sed voluptate officiis, aliquid accusantium dolorum cumque itaque expedita tenetur ipsa laboriosam, facere nisi similique. Mollitia, nobis.
                         </Div>
@@ -266,9 +314,9 @@ const Home: NextPage = () => {
                 </Row>
                 <Row h50></Row>
                 <Div hrTag h2 bgColor={"#605E5E"} mb50></Div>
-                <Row color={"#D3D3D3"}>
+                <Row color={"#D3D3D3"} >
                     <Col mb50>
-                        <Div fontSize40 pb36>Community Artist Royalty Program [C.A.R.P]</Div>
+                        <Div fontSize40 pb36 ref={carpRef}>Community Artist Royalty Program [C.A.R.P]</Div>
                         <Div fontSize18 fontFamily={bodyFontFamily}>
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit sequi iusto placeat voluptatem sed voluptate officiis, aliquid accusantium dolorum cumque itaque expedita tenetur ipsa laboriosam, facere nisi similique. Mollitia, nobis.
                         </Div>
@@ -280,7 +328,7 @@ const Home: NextPage = () => {
                 <Row h50></Row>
                 <Div hrTag h2 bgColor={"#605E5E"} mb50></Div>
                 <Row itemsCenter justifyCenter color={"#D3D3D3"}>
-                    <Div fontSize40 pb36 textCenter>Team</Div>
+                    <Div fontSize40 pb36 textCenter ref={teamRef}>Team</Div>
                     <Row>
                         <Col pb10>
                             <Div bgColor={"#A0A0A0"} p15 h330>
@@ -336,9 +384,9 @@ const Home: NextPage = () => {
                         </Col>
                     </Row>
                 </Row>
-                <Div hrTag h2 bgColor={"#605E5E"} m50></Div>
+                <Div hrTag h2 bgColor={"#605E5E"} m50 ref={faqRef}></Div>
                 <Row itemsCenter justifyCenter color={"#D3D3D3"} >
-                    <Col auto fontSize68 mr20 hFull>
+                    <Col auto fontSize68 mr20 >
                         FAQ
                     </Col>
                     <Col >
