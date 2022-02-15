fcfd3297d2780a464a6a84ea93cc7fef31fc1a46
Sehan MacPro <ianparkfirst@gmail.com>
2022-02-16 02:48:51 +0900

    feat: helmet

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,39 +1,39 @@
-import { Fragment, Suspense, useEffect, useMemo, useRef, useState } from "react";
+import { Fragment, Suspense, useEffect, useMemo, useRef, useState, useLayoutEffect } from "react";
 import Div from "src/components/Div";
 import Row from "src/components/Row";
 import Col from "src/components/Col";
-import { Canvas, useFrame, useLoader } from "@react-three/fiber";
+import { Canvas, useFrame, useLoader, useThree } from "@react-three/fiber";
 import { pagesWording } from "src/wording/pages";
 import EmptyBlock from "src/components/EmptyBlock";
 import Helmet from "react-helmet";
-import { Stars } from "@react-three/drei";
-import { Vector3, Color, TextureLoader, Vector2 } from "three";
+import { Stars, useAspect, OrbitControls, Image, useTexture } from "@react-three/drei";
+import { Vector3, Color, TextureLoader, Vector2, Euler } from "three";
 import { Controller, Scene } from "react-scrollmagic";
 import { Tween, Timeline } from "react-gsap";
+import { useGLTF } from "@react-three/drei";
 
-function Box(props) {
-	// This reference gives us direct access to the THREE.Mesh object
-	const ref = useRef(null);
-	// Hold state for hovered and clicked events
-	const [hovered, hover] = useState(false);
-	const [clicked, click] = useState(false);
-	// Subscribe this component to the render-loop, rotate the mesh every frame
-	useFrame((state, delta) => (ref.current.rotation.x += 0.01));
-	// Return the view, these are regular Threejs elements expressed in JSX
-	return (
-		<mesh
-			{...props}
-			ref={ref}
-			scale={clicked ? 1.5 : 1}
-			onClick={(event) => click(!clicked)}
-			onPointerOver={(event) => hover(true)}
-			onPointerOut={(event) => hover(false)}
-		>
-			<boxGeometry args={[1, 1, 1]} />
-			<meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
-		</mesh>
-	);
-}
+export const AstronautHelmet = () => {
+	const { scene, nodes, materials } = useGLTF("static/models/AstronautHelmet/scene.gltf");
+	const [colorMap, normalMap, emissiveMap] = useTexture([
+		"static/models/AstronautHelmet/textures/Helmet-Body_baseColor.jpeg",
+		"static/models/AstronautHelmet/textures/Helmet-Body_normal.png",
+		"static/models/AstronautHelmet/textures/Helmet-Body_emissive.png",
+	]);
+
+	// useLayoutEffect(() => {
+	// 	Object.assign(materials.Material, {
+	// 		//    metalnessMap: metalnessMap,
+	// 		normalMap: normalMap,
+	// 		//    roughnessMap: roughnessMap,
+	// 		emissiveMap: emissiveMap,
+	// 		map: colorMap,
+	// 		color: "white",
+	// 	});
+	// }, [scene, nodes, materials, colorMap, normalMap]);
+
+	return <primitive object={scene} />;
+};
+
 const RoundedButton = ({ size, color, text }) => {
 	let divProps = {} as any;
 	let textProps = {} as any;
@@ -86,20 +86,24 @@ const RoundedButton = ({ size, color, text }) => {
 	);
 };
 
-function CastAwayAstronaut({ from, to, speed }) {
+function CastAwayAstronaut({ from, to, speed, delay = 0 }) {
 	const texture = useLoader(TextureLoader, "https://beardom.s3.ap-northeast-2.amazonaws.com/avatars/emptyFaceAstronaut.png");
 	const [position, setPosition] = useState({ ...from, rotation: Math.PI });
 	const [stop, setStop] = useState(false);
 	useFrame((state, delta) => {
+		if (stop) return position;
+		if (state.clock.elapsedTime < delay) return position;
 		setPosition((prevPosition) => {
-			if (Math.abs(to.x - prevPosition.x) < 0.5 / speed && Math.abs(to.y - prevPosition.y) < 0.5 / speed) {
+			if (Math.abs(to.x - prevPosition.x) <= 0.1 && Math.abs(to.y - prevPosition.y) <= 0.1) {
 				setStop(true);
 			}
-			if (stop) return prevPosition;
+
+			const sin = Math.sin(state.clock.elapsedTime * speed);
+			const cos = Math.cos(state.clock.elapsedTime * speed);
 			return {
-				x: from.x + (from.x - to.x) * Math.sin(state.clock.elapsedTime) * speed,
-				y: from.y + (from.y - to.y) * Math.sin(state.clock.elapsedTime) * speed,
-				rotation: Math.sin(state.clock.elapsedTime),
+				x: from.x + (from.x - to.x) * sin,
+				y: from.y + (from.y - to.y) * sin,
+				rotation: sin * sin + cos * cos,
 			};
 		});
 	});
@@ -113,12 +117,81 @@ function CastAwayAstronaut({ from, to, speed }) {
 	);
 }
 
-function CanvasImage({ position, src, size }) {
-	const texture = useLoader(TextureLoader, src);
+const CanvasImage = ({ url, ...props }) => {
+	const texture = useMemo(() => new TextureLoader().load(url), [url]);
+
 	return (
-		<mesh position={new Vector3(position.x, position.y, 0)}>
-			<planeBufferGeometry attach="geometry" args={size} />
-			<meshBasicMaterial attach="material" map={texture} repeat={new Vector2(1, 1)} />
+		<mesh {...props}>
+			<planeBufferGeometry attach="geometry" args={[100, 100]} />
+			<meshLambertMaterial attach="material">
+				<primitive attach="map" object={texture} />
+			</meshLambertMaterial>
+		</mesh>
+	);
+};
+
+const getRadian = function (degrees) {
+	return (degrees * Math.PI) / 180;
+};
+
+const CanvasImages = () => {
+	return (
+		<>
+			{[
+				"static/images/1.png",
+				"static/images/2.png",
+				"static/images/3.png",
+				"static/images/4.png",
+				"static/images/5.png",
+				"static/images/6.png",
+				"static/images/1.png",
+				"static/images/2.png",
+				"static/images/3.png",
+				"static/images/4.png",
+				"static/images/5.png",
+				"static/images/6.png",
+				"static/images/1.png",
+				"static/images/2.png",
+				"static/images/3.png",
+				"static/images/4.png",
+				"static/images/5.png",
+				"static/images/6.png",
+				"static/images/1.png",
+				"static/images/2.png",
+				"static/images/3.png",
+			].map((url, index) => (
+				// @ts-ignore
+				<Image
+					key={index}
+					url={url}
+					scale={[10, 10]}
+					position-x={Math.sin(getRadian((index % 45) * 4.5) * 10) * 30}
+					position-y={-20 + index * 2}
+					rotation-y={Math.PI + index * (Math.PI / 4) * 1}
+					position-z={Math.cos(getRadian((index % 45) * 4.5) * 10) * 30}
+				/>
+			))}
+		</>
+	);
+};
+
+function CanvasVideo() {
+	const size = useAspect(900, 900);
+	const [video] = useState(() => {
+		const vid = document.createElement("video");
+		vid.src = "static/images/gomRoomz.mp4";
+		vid.crossOrigin = "Anonymous";
+		vid.loop = true;
+		return vid;
+	});
+	// Keep in mind videos can only play once the user has interacted with the site ...
+	useEffect(() => void video.play(), [video]);
+	return (
+		<mesh scale={size}>
+			<planeBufferGeometry />
+			<meshBasicMaterial>
+				<videoTexture attach="map" args={[video]} />
+			</meshBasicMaterial>
 		</mesh>
 	);
 }
@@ -141,31 +214,29 @@ const MovingSpotlight = ({ from, to, color }) => {
 	);
 };
 
-const Sphere = () => {
-	const planet = useRef(null);
-
-	return (
-		<mesh ref={planet} visible position={[0, 0, 0]}>
-			<sphereBufferGeometry attach="geometry" args={[1.5, 32, 32]} />
-			<meshStandardMaterial attach="material" color="#1f1f1f" />
-		</mesh>
-	);
-};
-
 export default function Home() {
 	const locale = "en";
+	const image = useRef(null);
+	function RotateCamera() {
+		useFrame((state) => {
+			state.camera.rotation.z += 0.02 * Math.sin(state.clock.elapsedTime * 0.8);
+			state.camera.rotation.y += 0.03 * Math.sin(state.clock.elapsedTime * 0.3);
+			state.camera.rotation.x += 0.005 * Math.cos(state.clock.elapsedTime * 0.2);
+			state.camera.updateProjectionMatrix();
+		});
+		return null;
+	}
+
 	return (
 		<Div bgBlack>
 			<Helmet bodyAttributes={{ style: "background-color : #000" }} />
 			<Div fixed hScreen wScreen>
 				<Canvas>
-					<Stars count={1000} />
-					<MovingSpotlight to={{ x: 24, y: 1 }} from={{ x: 19, y: 10 }} color={new Color(255, 0, 0)} />
-					<spotLight color={new Color(0, 255, 0)} intensity={0.01} position={new Vector3(0, 0, 40)} angle={Math.PI} penumbra={0} distance={50} />
-					{/* <Sphere /> */}
-					<pointLight position={new Vector3(0, 0, 0)} color={"green"} intensity={100} power={100} />
+					<Stars count={700} />
 					<Suspense fallback={null}>
-						<CastAwayAstronaut from={{ x: 0, y: 0 }} to={{ x: 10, y: 10 }} speed={0.8} />
+						<CastAwayAstronaut from={{ x: 0, y: 0 }} to={{ x: 10, y: 10 }} speed={0.2} />
+						<CastAwayAstronaut from={{ x: 0, y: 8 }} to={{ x: 10, y: -9 }} speed={0.3} delay={2} />
+						<CastAwayAstronaut from={{ x: -7, y: 6 }} to={{ x: 5, y: 10 }} speed={0.3} delay={2.2} />
 					</Suspense>
 				</Canvas>
 			</Div>
@@ -184,9 +255,18 @@ export default function Home() {
 						</Row>
 					</Col>
 					<Col></Col>
+					<Col auto>
+						<Div spanTag fontLight textWhite>
+							{locale}
+						</Div>
+					</Col>
 				</Row>
 			</Div>
-			<Div maxW={960} mxAuto textWhite>
+			<Div maxW={960} mxAuto textWhite relative>
+				<Div imgTag absolute src={"static/images/planets/11.png"} h80></Div>
+				<Div imgTag absolute src={"static/images/planets/13.png"} h30 right200 top50></Div>
+				<Div imgTag absolute src={"static/images/planets/18.png"} h100 left500 top300></Div>
+				<Div imgTag absolute src={"static/images/planets/19.png"} h40 left100 top600></Div>
 				<Controller>
 					<Scene duration={600} pin={{ pushFollowers: false }} triggerHook={0.5} offset={300}>
 						<Div>
@@ -197,7 +277,7 @@ export default function Home() {
 											<Timeline
 												target={
 													<Div textXxxl clx={"timeline"}>
-														{pagesWording.home.index.title[locale]}
+														{pagesWording.home.index.mintSection.title[locale]}
 													</Div>
 												}
 											>
@@ -206,7 +286,7 @@ export default function Home() {
 											<Timeline
 												target={
 													<Div textXxl clx={"timeline"} absolute top0>
-														{pagesWording.home.index.mainMessage[locale]}
+														{pagesWording.home.index.mintSection.mainMessage[locale]}
 													</Div>
 												}
 											>
@@ -216,29 +296,150 @@ export default function Home() {
 									)}
 								</Scene>
 							</Div>
-
 							<Div mxAuto maxW={500} textLg textCenter textGray500 bdBlurSm rounded3xl>
-								{pagesWording.home.index.secondaryMessage[locale]}
+								{pagesWording.home.index.mintSection.secondaryMessage[locale]}
 							</Div>
 							<Div mxAuto maxW={500} textBase textCenter textGray600 bdBlurSm rounded3xl>
-								{pagesWording.home.index.tertiaryMessage[locale]}
+								{pagesWording.home.index.mintSection.tertiaryMessage[locale]}
 							</Div>
 							<Div justifyCenter flex py25>
-								<RoundedButton size={"xlarge"} color={"black"} text={pagesWording.home.index.mintButton[locale]} />
+								<RoundedButton size={"xlarge"} color={"black"} text={pagesWording.home.index.mintSection.mintButton[locale]} />
+							</Div>
+							<EmptyBlock />
+							<Div>
+								<Scene duration={500} pin={{ pushFollowers: false }} triggerHook={0.5} offset={200}>
+									{(progress) => (
+										<Timeline totalProgress={progress} paused>
+											<Timeline
+												target={
+													<Div absolute h900 w900 right0>
+														<Row>
+															<Col auto>
+																<Div imgTag src={"static/images/gomzPlanet.png"} h400 w400></Div>
+															</Col>
+															<Col bdBlurSm rounded3xl>
+																<Div fontBold textXl textWhite mb15>
+																	{pagesWording.home.index.storySection.title[locale]}
+																</Div>
+
+																<Div textLg textGray500>
+																	{pagesWording.home.index.storySection.plot[locale]}
+																</Div>
+															</Col>
+														</Row>
+													</Div>
+												}
+											>
+												<Tween from={{ x: 1000 }} to={{ x: 0 }} />
+											</Timeline>
+										</Timeline>
+									)}
+								</Scene>
 							</Div>
 						</Div>
 					</Scene>
 				</Controller>
+			</Div>
+			<EmptyBlock h={600} />
+			<Controller>
+				<Div>
+					<Scene duration={300} pin={{ pushFollowers: false }} triggerHook={0.5} offset={1000}>
+						{(progress) => (
+							<Timeline totalProgress={progress} paused>
+								<Timeline
+									target={
+										<Div maxW={960} mxAuto>
+											<Row>
+												<Col bdBlurSm rounded3xl>
+													<Div fontBold textXl textWhite mb15>
+														{pagesWording.home.index.gallerySection.title[locale]}
+													</Div>
 
-				<EmptyBlock h={75} />
-			</Div>
-			<Div h500 wScreen>
-				<Canvas>
-					<MovingSpotlight to={{ x: 24, y: 1 }} from={{ x: 19, y: 10 }} color={new Color(255, 0, 0)} />
-					<spotLight color={new Color(0, 255, 0)} intensity={0.01} position={new Vector3(0, 0, 0)} angle={Math.PI} penumbra={0} distance={50} />
-					<Suspense fallback={null}>{/* <CanvasImage position={{ x: 0, y: 0 }} src={"static/images/1.png"} size={[5, 5]} /> */}</Suspense>
-				</Canvas>
-			</Div>
+													<Div textLg textGray500>
+														{pagesWording.home.index.gallerySection.plot[locale]}
+													</Div>
+												</Col>
+												<Col auto>
+													<Div h600 w400>
+														<Canvas camera={{ fov: 30, near: 1, far: 1000, position: [0, 120, 100] }}>
+															<OrbitControls enableZoom={false} />
+															<MovingSpotlight to={{ x: 24, y: 1 }} from={{ x: 19, y: 10 }} color={new Color(255, 0, 0)} />
+															<spotLight
+																color={new Color(0, 255, 0)}
+																intensity={0.01}
+																position={new Vector3(0, 0, 40)}
+																angle={Math.PI}
+																penumbra={0}
+																distance={50}
+															/>
+															<ambientLight intensity={0.1} />
+															<Suspense fallback={null}>
+																<AstronautHelmet />
+																{/* @ts-ignore */}
+																<Image url={"static/images/1cut.png"} ref={image} scale={[23, 23]} position-y={19} position-z={3} />
+																<RotateCamera />
+															</Suspense>
+														</Canvas>
+													</Div>
+												</Col>
+											</Row>
+										</Div>
+									}
+								>
+									<Tween from={{ x: -1000 }} to={{ x: 0 }} />
+								</Timeline>
+							</Timeline>
+						)}
+					</Scene>
+					{/* <Div>
+						<Scene duration={500} pin={{ pushFollowers: false }} triggerHook={0.5} offset={200}>
+							{(progress) => (
+								<Timeline totalProgress={progress} paused>
+									<Timeline
+										target={
+											<Div absolute h900 w900 right0>
+												<Row>
+													<Col auto>
+														<video width={600} height={400} autoPlay muted loop>
+															<source src="/gomRoomz.mp4" type="video/mp4" />
+														</video>
+													</Col>
+													<Col auto>
+														<Div>
+															<Div spanTag fontBold textWhite textXxl>
+																GomRoomz
+															</Div>
+														</Div>
+													</Col>
+												</Row>
+											</Div>
+										}
+									>
+										<Tween from={{ x: 1000 }} to={{ x: -400 }} />
+									</Timeline>
+								</Timeline>
+							)}
+						</Scene>
+					</Div> */}
+				</Div>
+			</Controller>
 		</Div>
 	);
+}
+
+{
+	/* <Div hScreen wScreeen relative>
+						<Canvas camera={{ fov: 30, near: 1, far: 1000, position: [0, 50, 70] }}>
+							<Suspense fallback={null}>
+								<OrbitControls enableDamping enablePan={false} enableZoom={false} dampingFactor={0.05} rotateSpeed={0.5} />
+								<CanvasImages />
+							</Suspense>
+						</Canvas>
+					</Div> */
+}
+{
+	/* <Col auto key={num} w200 relative>
+															<Div imgTag src={`static/images/emptyFaceBear.png`} z100 relative></Div>
+															<Div imgTag src={`static/images/${num}.png`} absolute w70 h70 rounded20 top40 left60></Div>
+														</Col> */
 }
--- src/wording/pages.tsx
+++ src/wording/pages.tsx
@@ -4,28 +4,70 @@ import Div from "src/components/Div";
 export const pagesWording = {
 	home: {
 		index: {
-			title: {
-				en: "GOMZ",
+			mintSection: {
+				title: {
+					en: "GOMZ",
+				},
+				mainMessage: {
+					en: "For a world without hard cheese.",
+				},
+				secondaryMessage: {
+					en: "Minting starts March 30, 2022.",
+				},
+				tertiaryMessage: {
+					en: (
+						<>
+							For inquieries or collaboration opportunities please contact{" "}
+							<Div spanTag textInfo>
+								tier0.momentum.kr@gmail.com
+							</Div>
+							.
+						</>
+					),
+				},
+				mintButton: {
+					en: "Mint",
+				},
 			},
-			mainMessage: {
-				en: "For a world without hard cheese.",
+			storySection: {
+				title: {
+					en: (
+						<>
+							Never lose hospitality,
+							<br /> even as space nomads.
+						</>
+					),
+				},
+				plot: {
+					en: (
+						<>
+							Greed-driven, planet-wide war has rendered Gomz&apos;s homes uninhabitable. 10,000 of these bears have turned to space with an
+							altruistic vision:{" "}
+							<Div textWhite textCenter py15>
+								Establish a world without hate by helping individuals that are in need.
+							</Div>{" "}
+							After years of deep hibernation in their space capsules, Gomz have been awakened by other space wanderers who are also in search of a
+							habitable planet. Instead of fighting these space wanderers, Gomz have decided to host parties for these strangers in their rooms
+							(GomRoomz) as the first testimony to their vision.
+						</>
+					),
+				},
 			},
-			secondaryMessage: {
-				en: "Minting starts March 30, 2022.",
-			},
-			tertiaryMessage: {
-				en: (
-					<>
-						For inquieries or collaboration opportunities please contact{" "}
-						<Div spanTag textInfo>
-							tier0.momentum.kr@gmail.com
-						</Div>
-						.
-					</>
-				),
-			},
-			mintButton: {
-				en: "Mint",
+			gallerySection: {
+				title: {
+					en: <>Gomz Nft</>,
+				},
+				plot: {
+					en: (
+						<>
+							All Gomz have pictures of themseleves back in their home planet, which they are willing to let you mint as an NFT. Visit the{" "}
+							<Div textInfo spanTag>
+								Gomz gallery
+							</Div>{" "}
+							for a preview.
+						</>
+					),
+				},
 			},
 		},
 	},
